380c2353a0b9244dab9542ee7bedb76a2be6c0be
Matheus Medeiros <mathmed22@gmail.com>
2022-07-31 11:06:14 -0300

    Alteracao, Inclusao e Exclusao de Cozinha - JPA

--- src/main/java/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
@@ -6,9 +6,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.List;
-
-public class InclusaoCozinhaMain {
+public class AlteracaoCozinhaMain {
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
                 .web(WebApplicationType.NONE)
@@ -17,15 +15,9 @@ public class InclusaoCozinhaMain {
         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
         Cozinha cozinha1 = new Cozinha();
+        cozinha1.setId(1L);
         cozinha1.setName("Brasileira");
 
-        Cozinha cozinha2 = new Cozinha();
-        cozinha2.setName("Japonesa");
-
-        cozinha1 = cadastroCozinha.salvar(cozinha1);
-        cozinha2 = cadastroCozinha.salvar(cozinha2);
-
-        System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getName());
-        System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getName());
+        cadastroCozinha.salvar(cozinha1);
     }
 }
