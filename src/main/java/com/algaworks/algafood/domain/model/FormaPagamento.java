414a993a647fb799d95eff87f1c99b3f9cd4c465
Matheus Medeiros <mathmed22@gmail.com>
2022-08-09 22:30:48 -0300

    Criaçao da entidade FormaPagamento

--- src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
+++ src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
@@ -2,24 +2,21 @@ package com.algaworks.algafood.domain.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
-import javax.persistence.*;
-import java.util.Objects;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Cozinha {
+public class FormaPagamento {
 
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String name;
-
+    private String descricao;
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,7 @@
-insert into cozinha (name) values ('Tailandesa');
-insert into cozinha (name) values ('Indiana');
-insert into restaurante (nome, taxa_frete) values ('Kekio Tai', 1);
-insert into restaurante (nome, taxa_frete) values ('Kazue Tai', 2);
-insert into restaurante (nome, taxa_frete) values ('Tomario Tai', 3);
+insert into cozinha (id, name) values (1, 'Tailandesa');
+insert into cozinha (id, name) values (2, 'Indiana');
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Kekio Tai', 10 , 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Kazue Tai', 9.50, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tomario Tai', 15, 2);
+insert  into forma_pagamento (descricao) values ('Foi pago com cartao de credito');
 
