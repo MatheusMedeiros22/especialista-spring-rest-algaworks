77cfa285be78569411328529bd46144d081830a3
Matheus Medeiros <mathmed22@gmail.com>
2022-08-09 22:37:42 -0300

    Criaçao da entidade estado

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,5 +3,8 @@ insert into cozinha (id, name) values (2, 'Indiana');
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Kekio Tai', 10 , 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Kazue Tai', 9.50, 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tomario Tai', 15, 2);
-insert  into forma_pagamento (descricao) values ('Foi pago com cartao de credito');
+insert into forma_pagamento (descricao) values ('Foi pago com cartao de credito');
+insert into forma_pagamento (descricao) values ('Foi pago com pix');
+insert into estado (nome) values ('Pernambuco');
+insert into estado (nome) values ('Amazonas');
 
